9315d88442d48751a9d67d2b3ba899d9c7fa897d
Elad Wagner <vn71632@walla.com>
2018-12-15 20:25:15 +0200

    upgrated main accomandation viewer

--- src/app/accommodation-pictures-viewer/accommodation-pictures-viewer.component.ts
+++ src/app/accommodation-pictures-viewer/accommodation-pictures-viewer.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-accommodation-pictures-viewer',
   templateUrl: './accommodation-pictures-viewer.component.html',
   styleUrls: ['./accommodation-pictures-viewer.component.css']
 })
-export class AccommodationPicturesViewerComponent implements OnInit {
+export class AccommodationPicturesViewerComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+	isViewInitiated = false;
+	RIGHT_BUTTON_CLICK_CODE = 0;
+	LEFT_BUTTON_CLICK_CODE = 1;
+	pictureSelectedIndex = 0;
+	PREVIEW_PICTURES = ['../../assets/previewPhotos/104033522.jpg', '../../assets/previewPhotos/115449694.jpg', '../../assets/previewPhotos/104137110.jpg', '../../assets/previewPhotos/104733470.jpg'];
+  	constructor() {
+		this.NUMBER_OF_PREVIEW_PICTURES = this.PREVIEW_PICTURES.length;
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {}
+	
+	ngAfterViewInit() {
+      	this.isViewInitaited = true;
+		document.getElementById('preview-picture').style.height = window.innerHeight - document.getElementById('bar-container').offsetHeight + "px";
+    }
+	
+	@HostListener('window:resize', ['$event'])
+	onResize(event) {
+		if(this.isViewInitaited) {
+			document.getElementById('preview-picture').style.height = window.innerHeight - document.getElementById('bar-container').offsetHeight + "px";
+		}
+	}
+
+	onArrowClick(buttonClickedCode) {
+		this.pictureSelectedIndex = ((((buttonClickedCode === this.RIGHT_BUTTON_CLICK_CODE ? 1 : -1) +  this.pictureSelectedIndex) % this.NUMBER_OF_PREVIEW_PICTURES)+ this.NUMBER_OF_PREVIEW_PICTURES) % this.NUMBER_OF_PREVIEW_PICTURES;
+		document.getElementById("preview-picture").style.backgroundImage = `url(\"${this.PREVIEW_PICTURES[this.pictureSelectedIndex]}\")`;
+	}
 
 }
